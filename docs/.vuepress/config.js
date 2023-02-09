36ec1a26bd4687af6032c81f4ebf0339fac31f07
liangxiao <liangxiao@dv-email.com>
2023-02-09 14:10:54 +0800

    打包部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { defaultTheme } from "vuepress";
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'lxblog',
-  base: '/blog',
+  base: './',
   description: '记录学习笔记的博客',
   theme: defaultTheme({
     logo: "/images/logo.jpg",
