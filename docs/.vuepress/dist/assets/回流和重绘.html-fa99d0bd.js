dc15f5e052cf10c1ac0810eee38e8c86eda437ef
liangxiao <liangxiao@dv-email.com>
2023-02-09 10:32:44 +0800

    打包项目

---
+++ docs/.vuepress/dist/assets/回流和重绘.html-fa99d0bd.js
@@ -0,0 +1,83 @@
+import{_ as n,p as s,q as a,a1 as t}from"./framework-204010b2.js";const e={},p=t(`<h2 id="_1-回流" tabindex="-1"><a class="header-anchor" href="#_1-回流" aria-hidden="true">#</a> 1：回流</h2><p>​ 当我们对 DOM 的修改引发了 DOM 几何尺寸的变化（比如修改元素的宽、高或隐藏元素等）时，浏览器需要重新计算元素的几何属性（其他元素的几何属性和位置也会因此受到影响），然后再将计算的结果绘制出来。这个过程就是回流（也叫重排）。</p><h2 id="_2-重绘" tabindex="-1"><a class="header-anchor" href="#_2-重绘" aria-hidden="true">#</a> 2：重绘</h2><p>​ 当我们对 DOM 的修改导致了样式的变化、却并未影响其几何属性（比如修改了颜色或背景色）时，浏览器不需重新计算元素的几何属性、直接为该元素绘制新的样式（跳过了上图所示的回流环节）。这个过程叫做重绘。</p><p><strong>重绘不一定导致回流，回流一定会导致重绘</strong></p><h2 id="_3-触发回流的操作" tabindex="-1"><a class="header-anchor" href="#_3-触发回流的操作" aria-hidden="true">#</a> 3：触发回流的操作</h2><ol><li><p>改变 DOM 元素的几何属性</p><p>当一个 DOM 元素的几何属性发生变化时，所有和它相关的节点（比如父子节点、兄弟节点等）的几何属性都需要进行重新计算，它会带来巨大的计算量。 常见的几何属性有 width、height、padding、margin、left、top、border 等等</p></li><li><p>改变 DOM 树的结构</p><p>主要指的是节点的增减、移动等操作</p></li><li><p>获取一些特定属性的值</p><p>当你要用到像这样的属性：offsetTop、offsetLeft、 offsetWidth、offsetHeight、scrollTop、scrollLeft、scrollWidth、scrollHeight、clientTop、clientLeft、clientWidth、clientHeight 时，你就要注意了！</p><p>“像这样”的属性，到底是像什么样？——这些值有一个共性，就是需要通过即时计算得到。因此浏览器为了获取这些值，也会进行回流。</p></li></ol><h2 id="_4-如何规避回流与重绘" tabindex="-1"><a class="header-anchor" href="#_4-如何规避回流与重绘" aria-hidden="true">#</a> 4：如何规避回流与重绘</h2><p>​ <strong>1：将“导火索”缓存起来，避免频繁改动</strong></p><p>​ 有时我们想要通过多次计算得到一个元素的布局位置，我们可能会这样做：</p><div class="language-html line-numbers-mode" data-ext="html"><pre class="language-html"><code><span class="token doctype"><span class="token punctuation">&lt;!</span><span class="token doctype-tag">DOCTYPE</span> <span class="token name">html</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>html</span> <span class="token attr-name">lang</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>en<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>head</span><span class="token punctuation">&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>meta</span> <span class="token attr-name">charset</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>UTF-8<span class="token punctuation">&quot;</span></span> <span class="token punctuation">/&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>meta</span> <span class="token attr-name">name</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>viewport<span class="token punctuation">&quot;</span></span> <span class="token attr-name">content</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>width=device-width, initial-scale=1.0<span class="token punctuation">&quot;</span></span> <span class="token punctuation">/&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>meta</span> <span class="token attr-name">http-equiv</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>X-UA-Compatible<span class="token punctuation">&quot;</span></span> <span class="token attr-name">content</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>ie=edge<span class="token punctuation">&quot;</span></span> <span class="token punctuation">/&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>title</span><span class="token punctuation">&gt;</span></span>Document<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>title</span><span class="token punctuation">&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>style</span><span class="token punctuation">&gt;</span></span><span class="token style"><span class="token language-css">
+      <span class="token selector">#el</span> <span class="token punctuation">{</span>
+        <span class="token property">width</span><span class="token punctuation">:</span> 100px<span class="token punctuation">;</span>
+        <span class="token property">height</span><span class="token punctuation">:</span> 100px<span class="token punctuation">;</span>
+        <span class="token property">background-color</span><span class="token punctuation">:</span> yellow<span class="token punctuation">;</span>
+        <span class="token property">position</span><span class="token punctuation">:</span> absolute<span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+    </span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>style</span><span class="token punctuation">&gt;</span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>head</span><span class="token punctuation">&gt;</span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>body</span><span class="token punctuation">&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">id</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>el<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>script</span><span class="token punctuation">&gt;</span></span><span class="token script"><span class="token language-javascript">
+      <span class="token comment">// 获取el元素</span>
+      <span class="token keyword">const</span> el <span class="token operator">=</span> document<span class="token punctuation">.</span><span class="token function">getElementById</span><span class="token punctuation">(</span><span class="token string">&quot;el&quot;</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+      <span class="token comment">// 这里循环判定比较简单，实际中或许会拓展出比较复杂的判定需求</span>
+      <span class="token keyword">for</span> <span class="token punctuation">(</span><span class="token keyword">let</span> i <span class="token operator">=</span> <span class="token number">0</span><span class="token punctuation">;</span> i <span class="token operator">&lt;</span> <span class="token number">10</span><span class="token punctuation">;</span> i<span class="token operator">++</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        el<span class="token punctuation">.</span>style<span class="token punctuation">.</span>top <span class="token operator">=</span> el<span class="token punctuation">.</span>offsetTop <span class="token operator">+</span> <span class="token number">10</span> <span class="token operator">+</span> <span class="token string">&quot;px&quot;</span><span class="token punctuation">;</span>
+        el<span class="token punctuation">.</span>style<span class="token punctuation">.</span>left <span class="token operator">=</span> el<span class="token punctuation">.</span>offsetLeft <span class="token operator">+</span> <span class="token number">10</span> <span class="token operator">+</span> <span class="token string">&quot;px&quot;</span><span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+    </span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>script</span><span class="token punctuation">&gt;</span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>html</span><span class="token punctuation">&gt;</span></span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>这样做，每次循环都需要获取多次“敏感属性”，是比较糟糕的。我们可以将其以 JS 变量的形式缓存起来，待计算完毕再提交给浏览器发出重计算请求</p><div class="language-javascript line-numbers-mode" data-ext="js"><pre class="language-javascript"><code><span class="token comment">// 缓存offsetLeft与offsetTop的值</span>
+<span class="token keyword">const</span> el <span class="token operator">=</span> document<span class="token punctuation">.</span><span class="token function">getElementById</span><span class="token punctuation">(</span><span class="token string">&quot;el&quot;</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token keyword">let</span> offLeft <span class="token operator">=</span> el<span class="token punctuation">.</span>offsetLeft<span class="token punctuation">,</span>
+  offTop <span class="token operator">=</span> el<span class="token punctuation">.</span>offsetTop<span class="token punctuation">;</span>
+
+<span class="token comment">// 在JS层面进行计算</span>
+<span class="token keyword">for</span> <span class="token punctuation">(</span><span class="token keyword">let</span> i <span class="token operator">=</span> <span class="token number">0</span><span class="token punctuation">;</span> i <span class="token operator">&lt;</span> <span class="token number">10</span><span class="token punctuation">;</span> i<span class="token operator">++</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+  offLeft <span class="token operator">+=</span> <span class="token number">10</span><span class="token punctuation">;</span>
+  offTop <span class="token operator">+=</span> <span class="token number">10</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment">// 一次性将计算结果应用到DOM上</span>
+el<span class="token punctuation">.</span>style<span class="token punctuation">.</span>left <span class="token operator">=</span> offLeft <span class="token operator">+</span> <span class="token string">&quot;px&quot;</span><span class="token punctuation">;</span>
+el<span class="token punctuation">.</span>style<span class="token punctuation">.</span>top <span class="token operator">=</span> offTop <span class="token operator">+</span> <span class="token string">&quot;px&quot;</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><strong>2：避免逐条改变样式，使用类名去合并样式</strong></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>const container = document.getElementById(&#39;container&#39;)
+container.style.width = &#39;100px&#39;
+container.style.height = &#39;200px&#39;
+container.style.border = &#39;10px solid red&#39;
+container.style.color = &#39;red&#39;
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>优化成一个有 class 加持的样子：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code> &lt;style&gt;
+    .basic_style {
+      width: 100px;
+      height: 200px;
+      border: 10px solid red;
+      color: red;
+    }
+  &lt;/style&gt;
+&lt;/head&gt;
+&lt;body&gt;
+  &lt;div id=&quot;container&quot;&gt;&lt;/div&gt;
+  &lt;script&gt;
+  const container = document.getElementById(&#39;container&#39;)
+  container.classList.add(&#39;basic_style&#39;)
+  &lt;/script&gt;
+&lt;/body&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>前者每次单独操作，都去触发一次渲染树更改，从而导致相应的回流与重绘过程。</p><p>合并之后，等于我们将所有的更改一次性发出，用一个 style 请求解决掉了。</p><p><strong>3：将 DOM 离线</strong></p><p>当我们需要对 DOM 对一系列修改的时候，可以通过以下步骤减少回流重绘次数：</p><p>1：使元素脱离文档流 2：对其进行多次修改 3: 将元素重新放回到文档中虽然该过程的第一步和第三步可能会引起回流，但是经过第一步之后，对 DOM 的所有修改都不会引起回流，因为它已经不在渲染树了。批量修改时可以使用这种方法。</p><p>有三种方式可以让 DOM 脱离文档流：</p><p>1: 隐藏元素：先把相应的元素给 display :none (有一次回流)，然后修改，接着再把它显示出来（有一次回流） 2: 使用文档片段(document fragment)在当前 DOM 之外构建一个子树，再把它拷贝回文档。 3: 将原始元素拷贝到一个脱离文档的节点中，修改节点后，再替换原始的元素。</p><p>仍以我们上文的代码片段为例：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>const container = document.getElementById(&#39;container&#39;)
+container.style.width = &#39;100px&#39;
+container.style.height = &#39;200px&#39;
+container.style.border = &#39;10px solid red&#39;
+container.style.color = &#39;red&#39;
+...（省略了许多类似的后续操作）
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>离线化后就是这样：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>let container = document.getElementById(&#39;container&#39;)
+container.style.display = &#39;none&#39;
+container.style.width = &#39;100px&#39;
+container.style.height = &#39;200px&#39;
+container.style.border = &#39;10px solid red&#39;
+container.style.color = &#39;red&#39;
+...（省略了许多类似的后续操作）
+container.style.display = &#39;block&#39;
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>有的同学会问，拿掉一个元素再把它放回去，这不也会触发一次昂贵的回流吗？这话不假，但我们把它拿下来了，后续不管我操作这个元素多少次，每一步的操作成本都会非常低。当我们只需要进行很少的 DOM 操作时，DOM 离线化的优越性确实不太明显。一旦操作频繁起来，这“拿掉”和“放回”的开销都将会是非常值得的。</p><p><strong>4：Flush 队列</strong></p><p>因为现代浏览器是很聪明的。浏览器自己也清楚，如果每次 DOM 操作都即时地反馈一次回流或重绘，那么性能上来说是扛不住的。于是它自己缓存了一个 flush 队列，把我们触发的回流与重绘任务都塞进去，待到队列里的任务多起来、或者达到了一定的时间间隔，或者“不得已”的时候，再将这些任务一口气出队</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,32),l=[p];function o(c,i){return s(),a("div",null,l)}const d=n(e,[["render",o],["__file","回流和重绘.html.vue"]]);export{d as default};
---
+++ docs/.vuepress/dist/assets/导航守卫.html-d2a67b16.js
@@ -0,0 +1,49 @@
+import{_ as e,p as n,q as r,a1 as i}from"./framework-204010b2.js";const t={},s=i(`<h2 id="_1-概念" tabindex="-1"><a class="header-anchor" href="#_1-概念" aria-hidden="true">#</a> 1：概念</h2><p>“ 导航 ” 表示路由正在发生变化 vue - router 提供的导航守卫主要用来通过跳转或取消的方式守卫导航。<strong>就是在路由跳转时加一些判断操作啥的</strong>，导航守卫：包括 <strong>全局导航守卫</strong> 和 <strong>局部导航守卫</strong></p><h2 id="_2-全局守卫" tabindex="-1"><a class="header-anchor" href="#_2-全局守卫" aria-hidden="true">#</a> 2：全局守卫</h2><p><strong>vue-router</strong> 全局有三个守卫</p><p><strong>router.beforeEach</strong>：全局前置守卫，进入路由之前</p><p><strong>router.beforeResolve</strong>：全局解析守卫，在 beforeRouteEnter 调用之后调用（不常用）</p><p><strong>router.afterEach</strong> ：全局后置钩子，进入路由之后</p><p><strong>全局前置钩子</strong></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>
+router.beforeEach((to, from, next) =&gt; {
+// to和from都是路由实例
+// to：即将跳转到的路由
+// from：现在的要离开的路由
+// next：函数
+})
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><strong>next: Function :</strong> 一定要调用该方法来 resolve 这个钩子。执行效果依赖 next 方法的调用参数。</p><p><strong>next() :</strong> 进行管道中的下一个钩子。如果全部钩子执行完了，则导航的状态就是 confirmed (确认的)。</p><p><strong>next(false) :</strong> 中断当前的导航。如果浏览器的 URL 改变了 (可能是用户手动或者浏览器后退按钮)，那么 URL 地址会重置到 from 路由对应的地址。</p><p><strong>next(&#39;/&#39;)</strong> 或者<strong>next({ path: &#39;/&#39; }) :</strong> 跳转到一个不同的地址。当前的导航被中断，然后进行一个新的导航。你可以向 next 传递任意位置对象，且允许设置诸如 replace: true 、 name: &#39;home&#39; 之类的选项以及任何用在 router-link 的 to prop 或 router.push 中的选项。</p><p><strong>next(error)</strong> : (2.4.0+) 如果传入 next 的参数是一个 Error 实例，则导航会被终止且该错误会 被传递给 router.onError() 注册过的回调。</p><p><strong>注意</strong>：如果是 next(&#39;/&#39;) 或者 next({ path: &#39;/&#39; }) ，只要带了要放行的路径，那么前面必须有判断，在什么时候给他放行，不然他会一直循环。</p><p><strong>全局后置钩子</strong></p><p>你也可以注册全局后置钩子，然而和守卫不同的是，这些钩子不会接受 next 函数也不会改变导航本身：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>// 全局后置钩子
+ router.afterEach((to,form) =&gt; { })
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_3-路由独享的守卫" tabindex="-1"><a class="header-anchor" href="#_3-路由独享的守卫" aria-hidden="true">#</a> 3：<strong>路由独享的守卫</strong></h2><p>如果不想在全局配置路由的话，可以为某些路由单独配置守卫</p><p>比如：给 mainpage 页面单独配置守卫</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>{
+path: &#39;/mainpage&#39;,
+name: &#39;About&#39;,
+component: About, // 路由独享守卫
+beforeEnter:(to,from,next) =&gt; {
+ if(from.name === &#39;About&#39;){ alert(&quot;这是从about来的&quot;)
+   }else{
+ alert(&quot;这不是从about来的&quot;)
+   }next(); // 必须调用来进行下一步操作。否则是不会跳转的
+ }
+}
+},
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_4-组件内的守卫" tabindex="-1"><a class="header-anchor" href="#_4-组件内的守卫" aria-hidden="true">#</a> <strong>4:组件内的守卫</strong></h2><p>最后，你可以在路由组件内直接定义以下路由导航守卫：</p><ul><li>beforeRouteEnter()：进入路由前</li><li>beforeRouteUpdate()：路由复用同一个组件时</li><li>beforeRouteLeave()：离开当前路由时</li></ul><p>在 Product 中举个例子</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>export default {
+// 组件内守卫beforeRouteUpdate被触发的条件是：当前路由改变，但是该组件被复用的时候。
+比如说：product/orders到product/cart这个路由，都复用了 Product.vue 这个组件，这个时候
+beforeRouteUpdate就会被触发。可以获取到this实例。
+
+// 因为这个钩子调用的时候，组件实例还没有被创建出来，因此获取不到this
+beforeRouteEnter (to, from, next) {
+console.log(to.name);
+// 如果想获取到实例的话
+// next(vm=&gt;{
+// // 这里的vm是组件的实例（this）
+// });
+next();
+},
+beforeRouteUpdate(to,from,next){
+console.log(to.name, from.name);
+next();
+},
+// 路由即将要离开的时候调用此方法
+// 比如说，用户编辑了一个东西，但是还么有保存，这时候他要离开这个页面，就要提醒他一下，还没保
+存，是否要离开
+beforeRouteLeave (to, from, next) {
+const leave = confirm(&quot;确定要离开吗？&quot;);
+if(leave) next() // 离开
+else next(false) // 不离开
+},
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_5-一个完整的导航解析流程" tabindex="-1"><a class="header-anchor" href="#_5-一个完整的导航解析流程" aria-hidden="true">#</a> 5:一个完整的导航解析流程</h2><p>1 、导航被触发。</p><p>2 、在失活的组件（即将离开的页面组件）里调用离开守卫。 beforeRouteLeave</p><p>3 、调用全局的 beforeEach 守卫。</p><p>4 、在重用的组件里调用 beforeRouteUpdate 守卫 ( 2.2 +) 。</p><p>5 、在路由配置里调用（路由独享的守卫） beforeEnter 。</p><p>6 、解析异步路由组件</p><p>7 、在被激活的组件（即将进入的页面组件）里调用 beforeRouteEnter 。</p><p>8 、调用全局的 beforeResolve 守卫 ( 2.5 +) 。</p><p>9 、导航被确认。</p><p>10 、调用全局的 afterEach 钩子。所有的钩子都触发完了。</p><p>11 、触发 DOM 更新。</p><p>12 、用创建好的实例调用 beforeRouteEnter 守卫中传给 next 的回调函数。</p>`,40),a=[s];function d(o,l){return n(),r("div",null,a)}const u=e(t,[["render",d],["__file","导航守卫.html.vue"]]);export{u as default};
